bfb817e35bdb8d1afeb800eee3f0a03a91089585
Charles Garraud <charles.garraud@inria.fr>
2016-02-02 17:21:35 +0100

    APP Scenario Player: Add copyright header to source files

--- applications/developer-tools/scenario-player/src/cvKernelFacade.cpp
+++ applications/developer-tools/scenario-player/src/cvKernelFacade.cpp
@@ -1,3 +1,26 @@
+/*********************************************************************
+* Software License Agreement (AGPL-3 License)                        *
+*                                                                    *
+* CertiViBE                                                          *
+* Based on OpenViBE V1.1.0, Copyright (C) INRIA, 2006-2015           *
+* Copyright (C) INRIA, 2015-2017,V1.0                                *
+*                                                                    *
+* \author Charles Garraud (INRIA)                                    *
+*                                                                    *
+* This program is free software: you can redistribute it and/or      *
+* modify it under the terms of the GNU Affero General Public License *
+* as published by the Free Software Foundation, either version 3 of  *
+* the License.                                                       *
+*                                                                    *
+* This program is distributed in the hope that it will be useful,    *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of     *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU   *
+* Affero General Public License for more details.                    *
+*                                                                    *
+* You should have received a copy of the GNU Affero General Public   *
+* License along with this program.                                   *
+* If not, see <http://www.gnu.org/licenses/>.                        *
+*********************************************************************/
 
 #include <cstdio>
 #include <map>
