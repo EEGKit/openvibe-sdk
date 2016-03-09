703110a4e2fecce0d74852f88a7114089e77307d
Jozef Legény <jozef.legeny@mensiatech.com>
2016-03-09 14:25:02 +0100

    Cleanup: Add back necessary files in file-io plugin

--- plugins/processing/file-io/src/ovp_main.cpp
+++ plugins/processing/file-io/src/ovp_main.cpp
@@ -6,10 +6,16 @@
 #include "algorithms/xml-scenario/ovpCAlgorithmXMLScenarioExporter.h"
 #include "algorithms/xml-scenario/ovpCAlgorithmXMLScenarioImporter.h"
 
+#include "box-algorithms/csv/ovpCBoxAlgorithmCSVFileWriter.h"
+#include "box-algorithms/csv/ovpCBoxAlgorithmCSVFileReader.h"
+
 #include "box-algorithms/openvibe/ovpCBoxAlgorithmGenericStreamReader.h"
 #include "box-algorithms/openvibe/ovpCBoxAlgorithmGenericStreamWriter.h"
 
 #include "box-algorithms/ovpCBoxAlgorithmElectrodeLocalizationFileReader.h"
+
+
+#include "box-algorithms/ovpCBoxAlgorithmSignalConcatenation.h"
 
 OVP_Declare_Begin()
 	OVP_Declare_New(OpenViBEPlugins::FileIO::CAlgorithmOVMatrixFileReaderDesc)
@@ -18,8 +24,14 @@ OVP_Declare_Begin()
 	OVP_Declare_New(OpenViBEPlugins::FileIO::CAlgorithmXMLScenarioExporterDesc)
 	OVP_Declare_New(OpenViBEPlugins::FileIO::CAlgorithmXMLScenarioImporterDesc)
 
+	OVP_Declare_New(OpenViBEPlugins::FileIO::CBoxAlgorithmCSVFileWriterDesc)
+	OVP_Declare_New(OpenViBEPlugins::FileIO::CBoxAlgorithmCSVFileReaderDesc)
+
 	OVP_Declare_New(OpenViBEPlugins::FileIO::CBoxAlgorithmGenericStreamReaderDesc)
 	OVP_Declare_New(OpenViBEPlugins::FileIO::CBoxAlgorithmGenericStreamWriterDesc)
 
 	OVP_Declare_New(OpenViBEPlugins::FileIO::CBoxAlgorithmElectrodeLocalisationFileReaderDesc)
+	
+	OVP_Declare_New(OpenViBEPlugins::FileIO::CBoxAlgorithmSignalConcatenationDesc)
+
 OVP_Declare_End()
