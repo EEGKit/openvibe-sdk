f4848ff7ee651418d97e9c274c7102a32724be65
Jozef Legény <jozef.legeny@mensiatech.com>
2016-03-09 14:01:22 +0100

    Cleanup: Move TimeSignal box to data-generation plugin

---
+++ plugins/processing/data-generation/src/ovp_defines.h
@@ -0,0 +1,24 @@
+#ifndef __OpenViBEPlugins_Defines_H__
+#define __OpenViBEPlugins_Defines_H__
+
+#define OVP_ClassId_TimeSignalGeneratorDesc                 OpenViBE::CIdentifier(0x57AD8655, 0x1966B4DC)
+//___________________________________________________________________//
+//                                                                   //
+// Plugin Object Class Identifiers                                   //
+//___________________________________________________________________//
+//                                                                   //
+
+#define OVP_ClassId_TimeSignalGenerator                     OpenViBE::CIdentifier(0x28A5E7FF, 0x530095DE)
+//___________________________________________________________________//
+//                                                                   //
+// Global defines                                                   //
+//___________________________________________________________________//
+//                                                                   //
+
+#ifdef TARGET_HAS_ThirdPartyOpenViBEPluginsGlobalDefines
+ #include "ovp_global_defines.h"
+#endif // TARGET_HAS_ThirdPartyOpenViBEPluginsGlobalDefines
+
+
+
+#endif // __OpenViBEPlugins_Defines_H__
---
+++ plugins/processing/data-generation/src/ovp_main.cpp
@@ -0,0 +1,8 @@
+#include "ovp_defines.h"
+
+#include "box-algorithms/ovpCTimeSignalGenerator.h"
+
+OVP_Declare_Begin();
+
+	OVP_Declare_New(OpenViBEPlugins::DataGeneration::CTimeSignalGeneratorDesc);
+OVP_Declare_End();
