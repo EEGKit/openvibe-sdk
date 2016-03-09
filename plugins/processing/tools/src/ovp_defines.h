854b47a7704c7821b957f78ccb350df50ecc5781
Jozef Legény <jozef.legeny@mensiatech.com>
2016-03-09 15:00:38 +0100

    Cleanup: Remove Cleanup: Remove the non-relevant documentation and boxes from tools plugin

--- plugins/processing/tools/src/ovp_defines.h
+++ plugins/processing/tools/src/ovp_defines.h
@@ -7,8 +7,6 @@
 //___________________________________________________________________//
 //                                                                   //
 
-#define OVP_ClassId_BoxAlgorithm_LatencyEvaluation          OpenViBE::CIdentifier(0x0AD11EC1, 0x7EF3690B)
-#define OVP_ClassId_BoxAlgorithm_LatencyEvaluationDesc      OpenViBE::CIdentifier(0x5DB56A54, 0x5380262B)
 
 //___________________________________________________________________//
 //                                                                   //
