81fb8c32d2b414afc25c8e6cf7ce7fbd7055bdf5
Charles Garraud <charles.garraud@inria.fr>
2016-05-23 14:25:41 +0200

    Clean: Clean Data Generation plugin
    Rename files/classes/definitions with BoxAlgorithm prefix
    Move box algorithms class definitions from ovp_defines_h to box algorithms header files.

--- plugins/processing/data-generation/src/ovp_main.cpp
+++ plugins/processing/data-generation/src/ovp_main.cpp
@@ -1,10 +1,10 @@
 #include "ovp_defines.h"
 
 
-#include "box-algorithms/ovpCTimeSignalGenerator.h"
+#include "box-algorithms/ovpCBoxAlgorithmTimeSignalGenerator.h"
 
 OVP_Declare_Begin();
 
-	OVP_Declare_New(OpenViBEPlugins::DataGeneration::CTimeSignalGeneratorDesc);
+	OVP_Declare_New(OpenViBEPlugins::DataGeneration::CBoxAlgorithmTimeSignalGeneratorDesc);
 
 OVP_Declare_End();
