f610aa341131e21547d27c0df0393bc89924fea2
Charles Garraud <charles.garraud@inria.fr>
2016-08-25 14:44:51 +0200

    [Box] Update error management in codec plugin
    [ref: https://jira.mensia.fr/browse/CORE-240]

--- plugins/processing/stream-codecs/src/algorithms/encoders/ovpCFeatureVectorEncoder.cpp
+++ plugins/processing/stream-codecs/src/algorithms/encoders/ovpCFeatureVectorEncoder.cpp
@@ -10,10 +10,11 @@ using namespace OpenViBEPlugins::StreamCodecs;
 
 boolean CFeatureVectorEncoder::processHeader(void)
 {
-	if(ip_pMatrix->getDimensionCount() != 1)
-	{
-		this->getLogManager() << LogLevel_Warning << "Trying to encode a feature vector with more than 1 dimension (" << ip_pMatrix->getDimensionCount() << "). This will violate the Feature Vector stream specification.\n";
-	}
+	OV_ERROR_UNLESS_KRF(
+		ip_pMatrix->getDimensionCount() == 1,
+		"Invalid feature vector: found " << ip_pMatrix->getDimensionCount() << " dimensions, 1 expected",
+		OpenViBE::Kernel::ErrorType::BadInput
+	);
 
 	CStreamedMatrixEncoder::processHeader();
 
