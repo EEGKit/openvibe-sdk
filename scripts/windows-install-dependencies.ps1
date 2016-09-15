dff7118525c92bec4f40f2e85b2456e252d3b57a
Charles Garraud <charles.garraud@inria.fr>
2016-09-15 12:08:18 +0200

    [Build] Windows dependencies install script and manifest files
    Tracker Reference: [Jire CORE-130](https://jira.mensiatech.com/browse/CORE-130)

---
+++ scripts/windows-install-dependencies.ps1
@@ -0,0 +1,203 @@
+<#
+.SYNOPSIS
+	This script aims at installing dependencies from a manifest file.
+.DESCRIPTION
+	Script workflow is as follows:
+	- Check for a cache directory
+	- Parse input dependencies manifest
+	- Download dependencies if:
+		* cache found but dependency not found in the cache
+		* cache not found (old archives are overwritten)
+	- Extract dependencies in destination folder (old dependencies are overwritten)
+	
+	Search for the cache directory is made through CV_DEPENDENCY_CACHE environment 
+	variable. If you maintain such a cache to synchronize local data with remote ones, 
+	please set CV_DEPENDENCY_CACHE. 
+	
+	Expected hierarchy of the cache directory is:
+	- '\dependencies' directory used to store project dependencies (binaries etc.)
+	- '\tests' used to store test data
+.PARAMETER dependencies_file
+	The manifest file containing the required archives to install. 
+	
+	Expected format is:
+	archive_name;folder_to_unzip;archive_url
+	archive_name;folder_to_unzip;archive_url
+	...
+.PARAMETER data_type
+	The type of data to install. This parameter is mainly used to look at the right directory
+	in the cache.
+	
+	Expected value: 'dependencies' or 'tests'.
+.PARAMETER dest_dir
+	Destination directory for extracted archives. Each archive found in the manifest file
+	is extracted in 'dest_dir\folder_to_unzip'.
+	
+	Note that if no cache is found, archives are also downloaded in 'dest_dir\arch'. 
+.NOTES
+	File Name      : windows-install-dependencies.ps1
+	Prerequisite   : Tested with PS v4.0 on windows 8.1 pro.
+.LINK
+	Detailed specifications:
+	https://jira.mensiatech.com/confluence/pages/viewpage.action?spaceKey=CT&title=Dependency+management
+.EXAMPLE
+	.\windows-install-dependencies.ps1 -dependencies_file \absolute\path\to\dep\dependencies.txt -data-type dependencies -dest_dir \absolute\path\to\dep\
+.EXAMPLE
+	powershell.exe -NoExit -NoProfile -ExecutionPolicy Bypass -File \absolute\path\to\windows-install-dependencies.ps1 -dependencies_file \absolute\path\to\dep\dependencies.txt -data_type dependencies -dest_dir \absolute\path\to\dep\
+#>
+
+#
+# script parameters
+#
+
+Param(
+[parameter(Mandatory=$true)][ValidateNotNullOrEmpty()][string]$dependencies_file,
+[parameter(Mandatory=$true)][ValidateSet('tests','dependencies', ignorecase=$False)][string]$data_type,
+[parameter(Mandatory=$true)][ValidateNotNullOrEmpty()][string]$dest_dir
+)
+
+#
+# header used to handle permission restrictions
+#
+
+If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator"))
+{
+	# manually forward the parameters
+	# generic way of forwarding with parsing of $myinvocation.Line failed because $myinvocation.Line is empty when the script is called from command line with powershell.exe
+    $argumentList = (" -dependencies_file", "`"$dependencies_file`"")
+	$argumentList += (" -data_type", "`"$data_type`"")
+	$argumentList += (" -dest_dir", "`"$dest_dir`"")
+	
+	$command = $myinvocation.MyCommand.Definition + $argumentList
+	Start-Process powershell.exe "-NoExit -NoProfile -ExecutionPolicy Bypass -File $command" -Verb RunAs
+	
+	Exit 
+}
+
+#
+# input validation
+#
+
+If (-Not (Test-Path $dependencies_file)){
+  Throw New-Object System.IO.FileNotFoundException "$dependencies_file not found" 
+}
+
+If (-Not (Test-Path $dest_dir)){
+  Throw New-Object System.IO.FileNotFoundException "$dest_dir not found" 
+}
+
+Write-Host "===Parameters==="
+Write-Host "Dependencies file = $dependencies_file"
+Write-Host "Destination directory = $dest_dir"
+Write-Host "Data type = $data_type"
+Write-Host ""
+
+#
+# script variables
+# 
+
+# we keep 2 different variables here because there is 2 different behaviors:
+# - cache archives shoud not be overwritten (It is the responsability of the system admin to keep it in sync with remote directories)
+# - destination directory archives must be overwritten to ensure a regular update
+$Script:cache_dir = ""
+$Script:arch_dir = ""
+
+# monitoring variables
+$Script:download_count = 0
+$Script:extract_count = 0
+
+#
+# script functions
+# 
+
+function DownloadDeps($url, $dest)
+{
+	Write-Host "Download: [" $url "] -> [" $dest "]"
+	
+	(New-Object Net.WebClient).DownloadFile($url, $dest)
+	 
+	$Script:download_count++
+}
+
+function ExpandZipFile($zip, $dest)
+{
+	Write-Host "Extract: [" $zip "] -> [" $dest "]"
+	$shell = New-Object -ComObject Shell.Application
+	
+	# create dest if it does not exists
+	If(-Not (Test-Path $dest)) {
+		New-Item $dest -itemtype directory | Out-Null
+	}
+	
+	# expand folders because NameSpace command expects absolute paths
+	$zip = (resolve-path $zip)
+	$dest = (resolve-path $dest)
+	
+	ForEach($item in $shell.NameSpace("$zip").items()) {
+		$shell.Namespace("$dest").copyhere($item, 0x14)
+	}
+	
+	$Script:extract_count++
+}
+
+function InstallDeps($arch, $dir, $url)
+{
+	$zip = $Script:arch_dir + "\" + $arch
+	
+	If(($Script:cache_dir -and -Not (Test-Path $zip)) -or (-Not $Script:cache_dir)) {
+		DownloadDeps $url $zip
+	}
+	
+	ExpandZipFile $zip ($Script:dest_dir + "\" + $dir)
+}
+
+#
+# core script
+#
+
+# check if a cache directory is available through CV_DEPENDENCY_CACHE env variable
+Write-Host "===Looking for cache==="
+If ((Test-Path env:\CV_DEPENDENCY_CACHE) -and (Test-Path $env:CV_DEPENDENCY_CACHE)) {
+	
+	Write-Host "Found cache directory: "  ($env:CV_DEPENDENCY_CACHE)
+	$cache_dir = $env:CV_DEPENDENCY_CACHE + "\" + $data_type
+	$arch_dir = $cache_dir
+	
+	If(-Not (Test-Path $cache_dir)) {
+		New-Item $cache_dir -itemtype directory | Out-Null
+		Write-Host "Created archive directory in cache: "  $cache_dir
+	}
+	
+	Write-Host "Missing archives will be transferred to " $arch_dir
+	
+} else {
+	Write-Host "Found no cache directory"
+	
+	$arch_dir = $dest_dir + "\arch" 
+	
+	If(-Not (Test-Path $arch_dir)) {
+		New-Item $arch_dir -itemtype directory | Out-Null
+		Write-Host "Created archive directory in destination: "  $arch_dir
+	}
+	
+	Write-Host "All archives will be transferred to " $arch_dir
+}
+
+Write-Host ""
+Write-Host "===Installing dependencies==="
+
+$Script:timer = [System.Diagnostics.Stopwatch]::StartNew()
+ForEach ($dep in (Get-Content $dependencies_file)) {
+	$arch, $dir, $url = $dep.split(';',3)
+	InstallDeps $arch $dir $url
+}
+$timer.Stop()
+
+
+Write-Host ""
+Write-Host "===Install Summary==="
+Write-Host "State = Success"
+Write-Host "Number of archives downloaded = $download_count" 
+Write-Host "Number of archives extracted = $extract_count"
+Write-Host "Installation time = "  $([string]::Format("{0:d2}h:{1:d2}mn:{2:d2}s", $timer.Elapsed.hours, $timer.Elapsed.minutes, $timer.Elapsed.seconds)) -nonewline
+Write-Host ""
