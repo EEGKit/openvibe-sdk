6bd4be262825e10044a19d0cea25ab2e91f39e31
Charles Garraud <charles.garraud@inria.fr>
2016-10-07 14:32:54 +0200

    [Box] Coding style refactoring in matrix multiplexer documentation
    Tracker Reference: [Jira CORE-284](https://jira.mensiatech.com/browse/CORE-284)

--- plugins/processing/streaming/src/box-algorithms/ovpCBoxAlgorithmStreamedMatrixMultiplexer.cpp
+++ plugins/processing/streaming/src/box-algorithms/ovpCBoxAlgorithmStreamedMatrixMultiplexer.cpp
@@ -9,42 +9,44 @@ using namespace OpenViBE::Plugins;
 using namespace OpenViBEPlugins;
 using namespace OpenViBEPlugins::Streaming;
 
-boolean CBoxAlgorithmStreamedMatrixMultiplexer::initialize(void)
+bool CBoxAlgorithmStreamedMatrixMultiplexer::initialize(void)
 {
-	m_ui64LastStartTime=0;
-	m_ui64LastEndTime=0;
-	m_bHeaderSent=false;
+	m_ui64LastStartTime = 0;
+	m_ui64LastEndTime = 0;
+	m_bHeaderSent = false;
 
 	return true;
 }
 
-boolean CBoxAlgorithmStreamedMatrixMultiplexer::uninitialize(void)
+bool CBoxAlgorithmStreamedMatrixMultiplexer::uninitialize(void)
 {
 	return true;
 }
 
-boolean CBoxAlgorithmStreamedMatrixMultiplexer::processInput(uint32 ui32InputIndex)
+bool CBoxAlgorithmStreamedMatrixMultiplexer::processInput(uint32 ui32InputIndex)
 {
 	getBoxAlgorithmContext()->markAlgorithmAsReadyToProcess();
 	return true;
 }
 
-boolean CBoxAlgorithmStreamedMatrixMultiplexer::process(void)
+bool CBoxAlgorithmStreamedMatrixMultiplexer::process(void)
 {
-	const IBox& l_rStaticBoxContext=this->getStaticBoxContext();
-	IBoxIO& l_rDynamicBoxContext=this->getDynamicBoxContext();
+	const IBox& l_rStaticBoxContext = this->getStaticBoxContext();
+	IBoxIO& l_rDynamicBoxContext = this->getDynamicBoxContext();
 
-	for(uint32 i=0; i<l_rStaticBoxContext.getInputCount(); i++)
+	for (uint32 i = 0; i < l_rStaticBoxContext.getInputCount(); i++)
 	{
-		for(uint32 j=0; j<l_rDynamicBoxContext.getInputChunkCount(i); j++)
+		for (uint32 j = 0; j < l_rDynamicBoxContext.getInputChunkCount(i); j++)
 		{
-			const IMemoryBuffer* l_pInputMemoryBuffer=l_rDynamicBoxContext.getInputChunk(i, j);
-			uint64 l_ui64StartTime=l_rDynamicBoxContext.getInputChunkStartTime(i, j);
-			uint64 l_ui64EndTime=l_rDynamicBoxContext.getInputChunkEndTime(i, j);
-			if((!m_bHeaderSent && l_ui64StartTime==l_ui64EndTime) || (m_bHeaderSent && l_ui64StartTime!=l_ui64EndTime))
+			const IMemoryBuffer* l_pInputMemoryBuffer = l_rDynamicBoxContext.getInputChunk(i, j);
+			uint64 l_ui64StartTime = l_rDynamicBoxContext.getInputChunkStartTime(i, j);
+			uint64 l_ui64EndTime = l_rDynamicBoxContext.getInputChunkEndTime(i, j);
+
+			if ((!m_bHeaderSent && l_ui64StartTime == l_ui64EndTime) || (m_bHeaderSent && l_ui64StartTime != l_ui64EndTime))
 			{
-				IMemoryBuffer* l_pOutputMemoryBuffer=l_rDynamicBoxContext.getOutputChunk(0);
+				IMemoryBuffer* l_pOutputMemoryBuffer = l_rDynamicBoxContext.getOutputChunk(0);
 				l_pOutputMemoryBuffer->setSize(l_pInputMemoryBuffer->getSize(), true);
+
 				System::Memory::copy(
 					l_pOutputMemoryBuffer->getDirectPointer(),
 					l_pInputMemoryBuffer->getDirectPointer(),
@@ -61,7 +63,7 @@ boolean CBoxAlgorithmStreamedMatrixMultiplexer::process(void)
 				m_ui64LastEndTime = l_ui64EndTime;
 
 				l_rDynamicBoxContext.markOutputAsReadyToSend(0, l_ui64StartTime, l_ui64EndTime);
-				m_bHeaderSent=true;
+				m_bHeaderSent = true;
 			}
 
 			l_rDynamicBoxContext.markInputAsDeprecated(i, j);
