d979f1b4fbd6efb3ace274004a67992415a2b90c
Thierry Gaugry <thierry.gaugry@inria.fr>
2017-03-10 15:12:04 +0100

    [Openvibe] Replace Boost functions by C++11 in CertiViBE
    Remove BOOST_STATIC_ASSERT
    Remove unused links to boost (Thread, Regex ...)
    
    Tracker Reference: [Jira CORE-578](https://jira.mensiatech.com/browse/CORE-578)

--- common/src/main.cpp
+++ common/src/main.cpp
@@ -1,8 +1,4 @@
 
-#ifdef TARGET_HAS_Boost
-#include <boost/static_assert.hpp>
-#endif
-
 // Appeasing Intellisense by including the following
 #include "ov_common_defines.h"
 
@@ -14,36 +10,7 @@ using namespace OpenViBETypeTest;
 
 int main(void) 
 {
-#ifdef TARGET_HAS_Boost
-	// static (on compile time) test of types here. 
-	// Note that these are legacy tests, kept until all compilers have moved to C++0x. 
-	BOOST_STATIC_ASSERT(sizeof(uint64)!=sizeof(uint32));
-	BOOST_STATIC_ASSERT(sizeof(uint64)!=sizeof(uint16));
-	BOOST_STATIC_ASSERT(sizeof(uint64)!=sizeof(uint8));
-	BOOST_STATIC_ASSERT(sizeof(uint32)!=sizeof(uint16));
-	BOOST_STATIC_ASSERT(sizeof(uint32)!=sizeof(uint8));
-	BOOST_STATIC_ASSERT(sizeof(uint16)!=sizeof(uint8));
-	
-	BOOST_STATIC_ASSERT(sizeof(int64)!=sizeof(int32));
-	BOOST_STATIC_ASSERT(sizeof(int64)!=sizeof(int16));
-	BOOST_STATIC_ASSERT(sizeof(int64)!=sizeof(int8));
-	BOOST_STATIC_ASSERT(sizeof(int32)!=sizeof(int16));
-	BOOST_STATIC_ASSERT(sizeof(int32)!=sizeof(int8));
-	BOOST_STATIC_ASSERT(sizeof(int16)!=sizeof(int8));
-	
-#ifndef TARGET_OS_Windows
-	BOOST_STATIC_ASSERT(sizeof(float80)!=sizeof(float64));
-	BOOST_STATIC_ASSERT(sizeof(float80)!=sizeof(float32));
-#endif
-	BOOST_STATIC_ASSERT(sizeof(float64)!=sizeof(float32));
-#endif
-
 #if defined(WIN32) || (defined(LINUX) && defined(__GXX_EXPERIMENTAL_CXX0X__))
-#ifndef static_assert
-	// On VS2008 we don't have static_assert(). Fine - we just skip these.
-#define static_assert(x,y) {} 
-#endif
-
 	static_assert(sizeof(uint64)>=8, "uint64 is not at least 8 bytes");
 	static_assert(sizeof(uint32)>=4, "uint32 is not at least 4 bytes");
 	static_assert(sizeof(uint16)>=2, "uint16 is not at least 2 bytes");
