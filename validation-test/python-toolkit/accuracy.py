ca0c63846c0f489403b5f91697e66b4ca919994d
Thierry Gaugry <thierry.gaugry@inria.fr>
2017-03-14 14:07:54 +0100

    [Validation Test] Use OVCSV box in tests instead of the old one
    Move python scripts to python-toolkit folder
    Update data files for the new csv format
    Update tests and scenarios for the new csv format
    Improve current tests
    
    Tracker Reference: [Jira CORE-577](https://jira.mensiatech.com/browse/CORE-577)

--- validation-test/python-toolkit/accuracy.py
+++ validation-test/python-toolkit/accuracy.py
@@ -18,7 +18,7 @@
 # along with this program.
 # If not, see <http://www.gnu.org/licenses/>.
 #######################################################################
-import sys, os
+import sys, os, re
 
 #arg 1 repository of the openvibe logs
 #arg 2 keyword to search in the log file to get the line where the measure was
@@ -29,15 +29,15 @@ threshold=float(sys.argv[3])
 
 #Function to select the line containing the keyword
 #the exception 103 is raised if file is missing
-def selectLineContainingWord (file,searchingWord):
+def selectLineContainingWord (filename, searchingWord):
     try:
-        for line in open(file):
-            for word in line.split():
-                if word ==searchingWord:
-                   selectedLine=line.split()
-                   return selectedLine
+        spaced_word = ' %s ' % searchingWord
+        with open(filename) as file :
+            for line in file:
+                if spaced_word in line :
+                    return line
 
-#Exception raised if the log file wasn't created        
+    #Exception raised if the log file wasn't created
     except FileNotFoundError:
         print('missing %s'%file)
         sys.exit(103)
@@ -45,12 +45,8 @@ def selectLineContainingWord (file,searchingWord):
 try:
     #Call the selectLineContainingWord function with the input parameter
     workingLine=selectLineContainingWord(openvibeLogs,keyWord)
-    
-    #Get the index of the word: is in the line 
-    getLineSize=workingLine.index('is')
-
-    #Get the measure and convert into float
-    getPercentageValue=float(workingLine[getLineSize+1].replace('%',''))
+	
+    getPercentageValue = float(re.search('accuracy\s+is\s+(\d+(?:\.\d+))\%', workingLine).group(1))
 
     #check if the measure is under the threshold excpected
     if getPercentageValue >= threshold:
@@ -74,10 +70,5 @@ except:
     raise
 
 sys.exit()
-
-
-
-
-
 
  
--- validation-test/python-toolkit/classification.py
+++ validation-test/python-toolkit/classification.py
@@ -33,129 +33,54 @@
 
 import csv
 import sys
+from itertools import islice
 
-
-try:
-    # assign arguments to variables
-    #arg 1 csv file recorded from ov file before the classification
-    #arg 2 csv file recorded after the classification
-    #arg 3 reference classification result
-    # open files associated with the varaibles
-    fileReferenceData = open(sys.argv[1], 'r')
-    fileTestData = open(sys.argv[2], 'r')
-    fileClassificationReference = open(sys.argv[3], 'r')
-
-
-    # use csv object to access the csv files
-    readerReferenceData = csv.reader(fileReferenceData, delimiter=';')
-    readerTestData = csv.reader(fileTestData, delimiter=';')
-
-    # Define all necessary list
-    inputData = []
-    outputData = []
-    referenceData = []
-    workingData = []
-    classificationReferenceData = []
-
-    # Convert classification reference file into list
-    # Iterator in python can't be reset,
-    # thus csv objects need to be converted to allow multiple iterations over the data
-    for fileIndex in fileClassificationReference:
-        classificationReferenceData.append(fileIndex)
-
-    # Convert input data before classification into list
-    # Iterator in python can't be reset,
-    # thus csv objects need to be converted to allow multiple iterations over the data
-    for rowReference in readerReferenceData:
-        inputData.append(rowReference)
-
-    # Convert output data after classification into list
-    # Iterator in python can't be reseted,
-    # csv object need to be converted to be iterate several times
-    # Use next() to start after the header of the csv file
-    next(readerTestData)
-    for rowReference in readerTestData:
-        outputData.append(rowReference)
-    # Check if outputdata lenght has a value
-    if len(outputData) <= 0:
-        print('The lenght of the test data: %s is equal to 0 or lower'%sys.argv[2])
-        sys.exit(109)
-    indexInputFile = 0
-
-    # Trick to add in the input file at the end of the list a timestamp reference
-    # to create the last time window
-    # Check if readerReferenceData line number has a value
-    if readerReferenceData.line_num <= 0:
+# assign arguments to variables
+#arg 1 csv file recorded from ov file before the classification
+#arg 2 csv file recorded after the classification
+#arg 3 reference classification result
+# open files associated with the varaibles
+with open(sys.argv[1], 'r') as fileReferenceData :
+    readerReferenceData = csv.reader(fileReferenceData, delimiter=',')
+    inputData = [rowReference for rowReference in readerReferenceData if rowReference[4] != ""]
+    if len(inputData) == 0:
         print('The lenght of the file reference data: %s is equal to 0 or lower'%sys.argv[1])
         sys.exit(109)
-    lastInputLine = inputData[readerReferenceData.line_num-1]
-
-    # Select into input data the signal with good stimulation marker
-    for rowReference in inputData:
-        if rowReference[1] == '33026' or rowReference[1] == '33027' or rowReference[1] == '33025':
-            referenceData.append(rowReference)
-    referenceData.append(lastInputLine)
-    # Check if the referenceData lenght has a value
-    if len(referenceData) <= 0:
-        print('The lenght of the reference data: %s is equal to 0 or lower'%sys.argv[2])
+with open(sys.argv[2], 'r') as fileTestData :
+    readerTestData = csv.reader(fileTestData, delimiter=',')
+    next(readerTestData)
+    outputData = [rowReference for rowReference in readerTestData if rowReference[3] != ""]
+    if len(outputData) == 0:
+        print('The lenght of the test data: %s is equal to 0 or lower'%sys.argv[2])
         sys.exit(109)
-    inputlength = len(referenceData)
+with open(sys.argv[3], 'r') as fileClassificationReference :
+    classificationReferenceData = [fileIndex for fileIndex in fileClassificationReference]
 
-    #  Check in a time window define by two timestamp the number of stimuation
-    while indexInputFile < (inputlength-1):
-        # Create the first timestamp referenc
-        firstLineReference = referenceData[indexInputFile]
-        stimulationReference = firstLineReference[1]
-        firstTimeReference = float(firstLineReference[0])
-        # Create the second timestamp reference
-        secondLineReference = referenceData[indexInputFile+1]
-        secondTimeReference = float(secondLineReference[0])
-        stimulationCount = 0
-        numberOfStimulation = 0
+# Select into input data the signal with good stimulation marker
+referenceData = [rowReference for rowReference in inputData if rowReference[3] in ['33026', '33027', '33025']]
+referenceData.append(inputData[-1])
 
-        # Create the time window between the first and second time stamp reference
-        #Count the reference stimulation ID
-        for index in outputData:
+#  Check in a time window define by two timestamp the number of stimuation
+for firstLineReference, secondLineReference, classificationLine in zip(referenceData, islice(referenceData, 1, None), classificationReferenceData) :
+    # Create the first timestamp referenc
+    stimulationReference = firstLineReference[3]
+    firstTimeReference = float(firstLineReference[0])
+    # Create the second timestamp reference
+    secondTimeReference = float(secondLineReference[0])
+    stimulationCount = 0
+    numberOfStimulation = 0
 
-            currentTimeReference = float(index[0])
-            if (currentTimeReference > firstTimeReference) \
-                and (currentTimeReference < secondTimeReference):
-                stimulationCount = stimulationCount + index.count(stimulationReference)
-                numberOfStimulation = numberOfStimulation + 1
-            elif currentTimeReference > secondTimeReference:
-                break
-        #Compute the percentage of success
-        classificationEvaluation = (stimulationCount/numberOfStimulation)*100
-        workingData = []
+    # Create the time window between the first and second time stamp reference
+    #Count the reference stimulation ID
+    ref_list = [index.count(stimulationReference) for index in outputData if firstTimeReference < float(index[0]) < secondTimeReference]
+    print(ref_list)
+    #Compute the percentage of success
+    classificationEvaluation = (sum(ref_list)/len(ref_list))*100
 
-        # Compare to the reference value
-        if abs(classificationEvaluation - float(classificationReferenceData[indexInputFile])) \
-            > sys.float_info.epsilon:
-
-            print('Classification refference value:%s'%classificationReferenceData[indexInputFile])
-            print('Classification evaluation value:%s'%classificationEvaluation)
-            sys.exit(108)
-        indexInputFile = indexInputFile+1
-
-# exception raised if missing file
-except FileNotFoundError as error:
-    print('Missing file:%s'%error.filename)
-
-# exception raise if output file empty
-except StopIteration:
-    print('error in output file:%s'%sys.argv[2])
-    sys.exit(110)
-
-except:
-    raise
-
-finally:
-    try:
-        fileReferenceData.close()
-        fileTestData.close()
-        fileClassificationReference.close()
-    # catch exception when close fail due to missing file
-    except NameError:
-        sys.exit(110)
+    # Compare to the reference value
+    if abs(classificationEvaluation - float(classificationLine)) > sys.float_info.epsilon:
+        print('Classification reference value:%s'%classificationLine)
+        print('Classification evaluation value:%s'%classificationEvaluation)
+        sys.exit(108)
 
 sys.exit()
