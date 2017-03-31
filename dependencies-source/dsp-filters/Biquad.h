5a5bd88fd3b90c29ae0006273ea8a26b4f0f533f
Jozef Legeny <jozef.legeny@mensiatech.com>
2017-03-31 11:34:53 +0200

    [Dependencies] Update DSP Filters dependency to fix High Pass filters with odd filter orders
    [CORE-605](https://jira.mensiatech.com/browse/CORE-605)

--- dependencies-source/dsp-filters/Biquad.h
+++ dependencies-source/dsp-filters/Biquad.h
@@ -81,8 +81,7 @@ public:
   template <class StateType, typename Sample>
   void process (int numSamples, Sample* dest, StateType& state) const
   {
-    while (--numSamples >= 0)
-    {
+    while (--numSamples >= 0) {
       *dest = state.process (*dest, *this);
       dest++;
     }
@@ -172,7 +171,8 @@ public:
       sectionPrev.m_b1 += db1;
       sectionPrev.m_b2 += db2;
 
-      *dest++ = state.process (*dest, sectionPrev);
+      *dest = state.process (*dest, sectionPrev);
+      dest++;
     }
   }
 
@@ -201,7 +201,8 @@ public:
       zPrev.zeros.second += dz1;
       zPrev.gain += dg;
 
-      *dest++ = state.process (*dest, Biquad (zPrev));
+      *dest = state.process (*dest, Biquad (zPrev));
+      dest++;
     }
   }
 
