a7a68ba27082d2072b836f5768d54159f621712f
Thierry Gaugry <thierry.gaugry@inria.fr>
2017-04-18 14:57:08 +0200

    [Build] Rename without -generic suffix
    Tracker Reference: [Jira CORE-595](https://jira.mensiatech.com/browse/CORE-595)

--- scripts/windows-install-dependencies-all.cmd
+++ scripts/windows-install-dependencies-all.cmd
@@ -1,3 +1,3 @@
-powershell.exe -NoProfile -ExecutionPolicy Bypass -file "windows-install-dependencies-generic.ps1" -manifest_file .\windows-dependencies.txt
-powershell.exe -NoProfile -ExecutionPolicy Bypass -file "windows-install-dependencies-generic.ps1" -manifest_file .\tests-data.txt
+powershell.exe -NoProfile -ExecutionPolicy Bypass -file "windows-install-dependencies.ps1" -manifest_file .\windows-dependencies.txt
+powershell.exe -NoProfile -ExecutionPolicy Bypass -file "windows-install-dependencies.ps1" -manifest_file .\tests-data.txt
 pause
