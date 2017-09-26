f966e82fc133082cd05e06b0ae7aead5fbc1ddb6
morgane-fauvet <morgane.fauvet@mensiatech.com>
2017-09-26 11:23:50 +0200

    [Scripts] Update unix-get-dependencies.sh and windows-get-dependencies.ps1 in order to download files from extranet dependencies platform. * Use 7zip executable to unzip archives in ps1 script, and update unix-get-dependencies.sh to make unzip quiet. * Rename windows-install-dependencies.ps1 as windows-get-dependencies.ps1 in order to uniformize naming with studio repository. * Remove url column from manifest and replace it by a version item. Version is read when unzipping dependencies in order to only unzip dependencies whose versions are not up-to-date.     * Move URL definition to environment variable, defined in windows-install-dependencies.cmd, and use one generic URL for data and build dependencies. * Edit file windows-install-dependencies.cmd make it copyable to windows-install-dependencies-custom.cmd and updated with right credentials, to store more easily credentials. * Update README.md file, to explain how to use the file `windows-install-dependencies-auth.cmd-skeleton` to install dependencies + Add version build number to generated zip package.
    Tracker Reference: [CORE-1084](https://jira.mensiatech.com/browse/CORE-1084)

--- INSTALL.md
+++ INSTALL.md
@@ -5,14 +5,16 @@ Linux Ubuntu (14.04 and 16.04)
 -----------------------------
 - Install the dependencies
   - run `linux-install-dependencies.pl` script; you will require sudo rights
-  - run `unix-get-dependencies.sh` script with an existing CV_DEPENDENCIES_CACHE environment variable or with --cache parameter (OPTIONAL - for tests)
+  - run `unix-get-dependencies.sh` script with an existing `DEPENDENCY_CACHE` environment variable or with --cache parameter (OPTIONAL - for tests), 
+  or with `PROXYPASS`
 
 
 Windows 7 and later
 -------------------
-- Install the dependencies
-  - run `windows-initialize-environment.cmd` script
-  - run `windows-install-dependencies-all.cmd` script
+- Install the dependencies:
+  - copy `windows-install-dependencies.cmd` and rename copy to `windows-install-dependencies-custom.cmd`
+  - edit line `set PROXYPASS=XXX:XXX` with appropriate username and password
+  - run it whenever you want to update dependencies.
 
 
 Install
--- scripts/windows-install-dependencies-all-x64.cmd
+++ scripts/windows-install-dependencies-all-x64.cmd
@@ -1,3 +1,23 @@
-powershell.exe -NoProfile -ExecutionPolicy Bypass -file "windows-install-dependencies.ps1" -manifest_file .\windows-dependencies-x64.txt -dest_dir .\..\dependencies_x64
-powershell.exe -NoProfile -ExecutionPolicy Bypass -file "windows-install-dependencies.ps1" -manifest_file .\tests-data.txt
+REM Install OV SDK dependencies
+REM This script can be copied, renamed as `windows-install-dependencies-custom.cmd`
+REM and updated with the right credentials in PROXYPASS, such as username:password
+REM 
+@echo off
+
+echo Install all dependencies
+
+REM In order to download dependencies, PROXYPASS must be changed with appropriate credentials
+REM set PROXYPASS=XXX:XXX
+set URL=https://extranet.mensiatech.com/dependencies
+
+if not exist "..\dependencies_x64\arch\build\windows" ( mkdir "..\dependencies_x64\arch\build\windows" )
+if not exist "..\dependencies\arch\data" ( mkdir "..\dependencies\arch\data" )
+
+REM Install build tools, CMake, Ninja, comment line if you already have an instance of CMake installed
+powershell.exe -NoProfile -ExecutionPolicy Bypass -file "windows-get-dependencies.ps1" -manifest_file .\windows-build-tools.txt -dest_dir .\..\dependencies_x64
+REM Install OV SDK dependencies
+powershell.exe -NoProfile -ExecutionPolicy Bypass -file "windows-get-dependencies.ps1" -manifest_file .\windows-dependencies-x64.txt -dest_dir .\..\dependencies_x64
+REM Retrieve test data
+powershell.exe -NoProfile -ExecutionPolicy Bypass -file "windows-get-dependencies.ps1" -manifest_file .\tests-data.txt
+
 pause
