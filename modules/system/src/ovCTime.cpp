5300a67a6d464e9868879e39df3c8083e56f01d7
Thierry Gaugry <thierry.gaugry@inria.fr>
2017-10-17 10:22:30 +0200

    [Kernel] Improve time precision with vs2013
    Reintroduce boost::chrono and boost::thread in ovCTime Class
    Minor types update
    
    Tracker Reference: [CORE-1113](https://jira.mensiatech.com/browse/CORE-1113)

--- modules/system/src/ovCTime.cpp
+++ modules/system/src/ovCTime.cpp
@@ -20,10 +20,11 @@
 
 #ifdef BOOST_CHRONO_HAS_CLOCK_STEADY
 
-#include <chrono>
-#include <thread>
+#include <boost/chrono.hpp>
+#include <boost/thread.hpp>
+using internal_clock = boost::chrono::steady_clock;
 using namespace std;
-
+#define FORCE_BOOST_CHRONO
 #endif // BOOST_CHRONO_HAS_CLOCK_STEADY
 
 #endif // TARGET_HAS_Boost_Chrono
@@ -34,37 +35,45 @@ using namespace std;
 #include <thread>
 using namespace std;
 
+using internal_clock = chrono::steady_clock;
+// using internal_clock = chrono::high_resolution_clock;
+
 #endif // defined(_MSC_VER) && _MSC_VER <= 1800
 
-using internal_clock = chrono::steady_clock;
-
-bool System::Time::sleep(const uint32 ui32MilliSeconds)
+bool System::Time::sleep(const uint32_t ui32MilliSeconds)
 {
+
+#ifdef FORCE_BOOST_CHRONO
+	const boost::posix_time::time_duration l_oDuration = boost::posix_time::milliseconds(ui32MilliSeconds);
+	boost::this_thread::sleep(l_oDuration);
+#else
 	this_thread::sleep_for(chrono::milliseconds(ui32MilliSeconds));
-
+#endif
 	return true;
 }
 
-bool System::Time::zsleep(const uint64 ui64Seconds)
+bool System::Time::zsleep(const uint64_t ui64Seconds)
 {
-	const uint32 l_ui32Seconds = static_cast<uint32>(ui64Seconds >> 32);
+	const uint32_t l_ui32Seconds = static_cast<uint32_t>(ui64Seconds >> 32);
 	// zero the seconds with 0xFFFFFFFF, multiply to get the rest as fixed point microsec, then grab them (now in the 32 msbs)
-	const uint64 l_ui64MicroSeconds = ((ui64Seconds & 0xFFFFFFFFLL) * 1000000LL) >> 32;
-	
+	const uint64_t l_ui64MicroSeconds = ((ui64Seconds & 0xFFFFFFFFLL) * 1000000LL) >> 32;
+#ifdef FORCE_BOOST_CHRONO
+	const boost::posix_time::time_duration l_oDuration = boost::posix_time::seconds(l_ui32Seconds) + boost::posix_time::microsec(l_ui64MicroSeconds);
+	boost::this_thread::sleep(l_oDuration);
+#else
 	chrono::microseconds l_oDuration = chrono::seconds(l_ui32Seconds) + chrono::microseconds(l_ui64MicroSeconds);
-
 	this_thread::sleep_for(l_oDuration);
-
+#endif
 	return true;
 }
 
-System::uint32 System::Time::getTime(void)
+uint32_t System::Time::getTime(void)
 {
 	// turn the 32:32 fixed point seconds to milliseconds
-	return static_cast<uint32>((zgetTime() * 1000) >> 32);
+	return static_cast<uint32_t>((zgetTime() * 1000) >> 32);
 }
 
-System::uint64 System::Time::zgetTime(void)
+uint64_t System::Time::zgetTime(void)
 {
 	static bool l_bInitialized = false;
 	static internal_clock::time_point l_oTimeStart;
@@ -79,10 +88,12 @@ System::uint64 System::Time::zgetTime(void)
 	
 	const internal_clock::duration l_oElapsed = l_oTimeNow - l_oTimeStart;
 
+#ifdef FORCE_BOOST_CHRONO
+	const boost::chrono::microseconds l_oElapsedMs = boost::chrono::duration_cast<boost::chrono::microseconds>(l_oElapsed);
+#else
 	const chrono::microseconds l_oElapsedMs = chrono::duration_cast<chrono::microseconds>(l_oElapsed);
-
+#endif
 	const uint64_t l_ui64MicrosPerSecond = 1000ULL * 1000ULL;
-
 	const uint64_t l_ui64Seconds = static_cast<uint64_t>(l_oElapsedMs.count() / l_ui64MicrosPerSecond);
 	const uint64_t l_ui64Fraction = static_cast<uint64_t>(l_oElapsedMs.count() % l_ui64MicrosPerSecond);
 
@@ -97,11 +108,10 @@ bool System::Time::isClockSteady()
 	return internal_clock::is_steady;
 }
 
-bool System::Time::checkResolution(const System::uint32 ui32MilliSeconds)
+bool System::Time::checkResolution(const uint32_t ui32MilliSeconds)
 {
 	assert(ui32MilliSeconds != 0);
 
 	auto l_ui32Resolution = static_cast<System::float64>(internal_clock::period::num) / internal_clock::period::den;
-
-	return (static_cast<System::uint32>(std::ceil(l_ui32Resolution * 1000)) <= ui32MilliSeconds);
+	return (static_cast<uint32_t>(std::ceil(l_ui32Resolution * 1000)) <= ui32MilliSeconds);
 }
