705d1a48382d7e0afc3515b7deadefebecf07301
Jozef Legény <jozef.legeny@mensiatech.com>
2018-06-18 13:58:19 +0200

    [Kernel] Check boolean settings
    - also evaluate integer settings completely to avoid crashes when the
      settings uses lepton variables (e.g.: 1+a)
    - update type error messages

--- kernel/src/tools/ovk_setting_checker.h
+++ kernel/src/tools/ovk_setting_checker.h
@@ -3,8 +3,22 @@
 #include "lepton/Lepton.h"
 #include "../ovk_base.h"
 
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <functional>
+
 namespace
 {
+	// because std::tolower has multiple signatures,
+	// it can not be easily used in std::transform
+	// this workaround is taken from http://www.gcek.net/ref/books/sw/cpp/ticppv2/
+	template <class charT>
+	charT to_lower(charT c)
+	{
+		return std::tolower(c);
+	}
+
 	/**
 	* \brief Check the setting value (if the setting is numeric),
 	* test if it is a correct arithmetic expression, should be used after
@@ -23,9 +37,19 @@ namespace
 			// parse and expression with no variables or functions
 			try
 			{
-				Lepton::Parser::parse(value.toASCIIString());
+				Lepton::Parser::parse(value.toASCIIString()).evaluate();
 			}
 			catch (...)
+			{
+				return false;
+			}
+		}
+		else if (typeIdentifier == OV_TypeId_Boolean)
+		{
+			std::string val = value.toASCIIString();
+			std::transform(val.begin(), val.end(), val.begin(), ::to_lower<std::string::value_type>);
+
+			if (!(val == "true" || val == "on" || val == "1" || val == "false" || val == "off" || val == "0"))
 			{
 				return false;
 			}
@@ -33,5 +57,6 @@ namespace
 		//TODO: else
 		return true;
 	}
+
 };
 
