b797fb7610270b83bdac0392c3db3bfeab8f365f
Thibaut <thibaut.monseigne@inria.fr>
2020-03-06 09:56:35 +0100

    Fix : Id Generator

--- applications/developer-tools/id-generator/src/ovig_main.cpp
+++ applications/developer-tools/id-generator/src/ovig_main.cpp
@@ -10,12 +10,13 @@ int main(int /*argc*/, char** /*argv*/)
 	std::random_device rd;
 	std::default_random_engine rng(rd());
 	std::uniform_int_distribution<size_t> uni(0, std::numeric_limits<size_t>::max() - 1);
-	std::stringstream ss;
-	ss.fill('0');
+
 
 	for (int i = 0; i < 16; ++i)
 	{
-		const size_t v1 = uni(rng), v2 = uni(rng);
+		const uint32_t v1 = uni(rng), v2 = uni(rng);
+		std::stringstream ss;
+		ss.fill('0');
 		ss << "(0x" << std::setw(8) << std::hex << v1 << ", 0x" << std::setw(8) << std::hex << v2 << ")";
 		std::cout << "#define OV_ClassId_\t\t\tOpenViBE::CIdentifier" << ss.str() << std::endl;
 	}
