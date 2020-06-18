1930f82761e7e4acde84d8991c6cb9d4382642fa
Thibaut <thibaut.monseigne@inria.fr>
2020-06-18 16:11:33 +0200

    :penguin: unix fix

--- openvibe/include/openvibe/CStimulation.hpp
+++ openvibe/include/openvibe/CStimulation.hpp
@@ -10,6 +10,7 @@
 ///-------------------------------------------------------------------------------------------------
 #pragma once
 
+#include <iostream>
 #include "ov_common_defines.h"
 #include "CTime.hpp"
 
