65edfa0e7a04bba6795c9f01ec44c8d122811650
Thibaut Monseigne <thibaut.monseigne@inria.fr>
2022-06-10 10:21:47 +0200

    :recycle: Update Unit Tests

--- unit-test/openvibe-module-fs/uoFilesTest.cpp
+++ unit-test/openvibe-module-fs/uoFilesTest.cpp
@@ -30,58 +30,55 @@ int uoFilesTest(int /*argc*/, char* argv[])
 {
 	std::string outputDirectory = argv[1];
 
-	OVT_ASSERT(FS::Files::directoryExists(outputDirectory.c_str()), "Failure to find test data directory");
+	OVT_ASSERT(FS::Files::DirectoryExists(outputDirectory), "Failure to find test data directory");
 
 	std::string testDir  = outputDirectory + "uoFilesTest";
 	std::string testFile = testDir + "/uoFilesTest.txt";
 
 	// test path creation
 
-	FS::Files::createParentPath(testFile.c_str());
-	OVT_ASSERT(FS::Files::directoryExists(testDir.c_str()), "Failure to create directory");
-	OVT_ASSERT(!FS::Files::fileExists(testFile.c_str()), "Failure to create parent path only");
+	FS::Files::CreateParentPath(testFile);
+	OVT_ASSERT(FS::Files::DirectoryExists(testDir), "Failure to create directory");
+	OVT_ASSERT(!FS::Files::FileExists(testFile), "Failure to create parent path only");
 
 	// test path retrieval methods
 
-	char parentPathFromFile[1024];
-	FS::Files::getParentPath(testFile.c_str(), parentPathFromFile);
-	OVT_ASSERT_STREQ(std::string(parentPathFromFile), testDir, "Failure to retrieve parent path from full path");
+	std::string parentPathFromFile = FS::Files::GetParentPath(testFile);
+	OVT_ASSERT_STREQ(parentPathFromFile, testDir, "Failure to retrieve parent path from full path");
 
-	char parentPathFromDir[1024];
-	FS::Files::getParentPath(testDir.c_str(), parentPathFromDir);
+	std::string parentPathFromDir = FS::Files::GetParentPath(testDir);
 	std::cout << parentPathFromDir << " " << outputDirectory << std::endl;
-	OVT_ASSERT_STREQ((std::string(parentPathFromDir) + "/"), outputDirectory, "Failure to retrieve parent path from path with no slash");
+	OVT_ASSERT_STREQ((parentPathFromDir + "/"), outputDirectory, "Failure to retrieve parent path from path with no slash");
 
 	testDir += "/"; // adding a slash should now give testDir as parent
-	FS::Files::getParentPath(testDir.c_str(), parentPathFromDir);
-	OVT_ASSERT_STREQ((std::string(parentPathFromDir) + "/"), testDir, "Failure to retrieve parent path from path with slash");
+	parentPathFromDir = FS::Files::GetParentPath(testDir);
+	OVT_ASSERT_STREQ((parentPathFromDir + "/"), testDir, "Failure to retrieve parent path from path with slash");
 
-	char filename[256];
-	FS::Files::getFilename(testFile.c_str(), filename);
-	OVT_ASSERT_STREQ(std::string(filename), std::string("uoFilesTest.txt"), "Failure to retrieve filename from full path");
+	std::string filename = FS::Files::GetFilename(testFile);
+	OVT_ASSERT_STREQ(filename, std::string("uoFilesTest.txt"), "Failure to retrieve filename from full path");
 
-	FS::Files::getFilenameWithoutExtension(testFile.c_str(), filename);
+	filename = FS::Files::GetFilenameWithoutExtension(testFile);
 	OVT_ASSERT_STREQ(std::string(filename), std::string("uoFilesTest"), "Failure to retrieve filename with no extension from full path");
 
 	// test file creation and opening
 
 	std::ofstream ostream;
-	FS::Files::openOFStream(ostream, testFile.c_str());
-	OVT_ASSERT(FS::Files::fileExists(testFile.c_str()), "Failure to create file");
+	FS::Files::OpenOFStream(ostream, testFile);
+	OVT_ASSERT(FS::Files::FileExists(testFile), "Failure to create file");
 	OVT_ASSERT(ostream.is_open(), "Failure to open file");
 	ostream.close();
 
 	std::string testFileInMissingnDir = testDir + "/newDir/uoFilesTest.txt";
-	FS::Files::openOFStream(ostream, testFileInMissingnDir.c_str());
-	OVT_ASSERT(!FS::Files::fileExists(testFileInMissingnDir.c_str()), "Failure to check for non-existing file");
+	FS::Files::OpenOFStream(ostream, testFileInMissingnDir);
+	OVT_ASSERT(!FS::Files::FileExists(testFileInMissingnDir), "Failure to check for non-existing file");
 
 	std::ifstream istream;
-	FS::Files::openIFStream(istream, testFile.c_str());
+	FS::Files::OpenIFStream(istream, testFile);
 	OVT_ASSERT(istream.is_open(), "Failure to open file in an input stream");
 	istream.close();
 
 	std::fstream fstream;
-	FS::Files::openFStream(fstream, testFile.c_str(), std::ios_base::out);
+	FS::Files::OpenFStream(fstream, testFile, std::ios_base::out);
 	OVT_ASSERT(fstream.is_open(), "Failure to open file in a generic stream");
 	fstream.close();
 
@@ -92,8 +89,8 @@ int uoFilesTest(int /*argc*/, char* argv[])
 	OVT_ASSERT(file == nullptr, "Failure to return NULL FILE object for non-existing file");
 
 	testDir = outputDirectory + "uoFilesTest2/long spaced/path";
-	FS::Files::createPath(testDir.c_str());
-	OVT_ASSERT(FS::Files::directoryExists(testDir.c_str()), "Failure to create directory with path containing spaces");
+	FS::Files::CreatePath(testDir);
+	OVT_ASSERT(FS::Files::DirectoryExists(testDir), "Failure to create directory with path containing spaces");
 
 	// test equality
 
@@ -102,26 +99,27 @@ int uoFilesTest(int /*argc*/, char* argv[])
 
 
 	// test folder copy
-
 	std::string testFile2       = outputDirectory + "uoFilesTest" + "/uoFilesTestChild/uoFilesTest.txt";
 	std::string testTargetDir   = outputDirectory + "uoFilesTestCopy";
 	std::string testTargetFile1 = testTargetDir + "/uoFilesTest.txt";
 	std::string testTargetFile2 = testTargetDir + "/uoFilesTestChild/uoFilesTest.txt";
+
 	// create a subfolder with file
-	FS::Files::createParentPath(testFile2.c_str());
+	FS::Files::CreateParentPath(testFile2);
 	std::ofstream ostream2;
-	FS::Files::openOFStream(ostream2, testFile2.c_str());
-	OVT_ASSERT(FS::Files::fileExists(testFile2.c_str()), "Failure to create file in subfolder");
+	FS::Files::OpenOFStream(ostream2, testFile2);
+	OVT_ASSERT(FS::Files::FileExists(testFile2), "Failure to create file in subfolder");
 	OVT_ASSERT(ostream2.is_open(), "Failure to open file");
 	ostream2.close();
+
 	// copy folder
 	testDir = outputDirectory + "uoFilesTest";
-	FS::Files::copyDirectory(testDir.c_str(), testTargetDir.c_str());
-	OVT_ASSERT(FS::Files::directoryExists(testTargetDir.c_str()), "Failure in copying folder");
-	OVT_ASSERT(FS::Files::fileExists(testTargetFile1.c_str()), "Failure in copying child files of the folder");
-	OVT_ASSERT(FS::Files::fileExists(testTargetFile2.c_str()), "Failure in copying subfolder");
+	FS::Files::CopyDirectory(testDir, testTargetDir);
+	OVT_ASSERT(FS::Files::DirectoryExists(testTargetDir), "Failure in copying folder");
+	OVT_ASSERT(FS::Files::FileExists(testTargetFile1), "Failure in copying child files of the folder");
+	OVT_ASSERT(FS::Files::FileExists(testTargetFile2), "Failure in copying subfolder");
 	// test folder copy on existing folder
-	OVT_ASSERT(!FS::Files::copyDirectory(testDir.c_str(), testTargetDir.c_str()), "Failure: Copy should not have been done if folder exits");
+	OVT_ASSERT(!FS::Files::CopyDirectory(testDir, testTargetDir), "Failure: Copy should not have been done if folder exits");
 
 	return EXIT_SUCCESS;
 }
