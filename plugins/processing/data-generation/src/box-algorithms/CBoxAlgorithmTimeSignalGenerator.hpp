0ef23e296629ceec1410a450d93a46e76d9533db
MONSEIGNE Thibaut <thibaut.monseigne@inria.fr>
2022-07-28 08:00:46 +0000

    :rotating_light: Resolve "Update: Data generation Clang and Coding Rules"

--- plugins/processing/data-generation/src/box-algorithms/CBoxAlgorithmTimeSignalGenerator.hpp
+++ plugins/processing/data-generation/src/box-algorithms/CBoxAlgorithmTimeSignalGenerator.hpp
@@ -1,6 +1,29 @@
+///-------------------------------------------------------------------------------------------------
+/// 
+/// \file CBoxAlgorithmTimeSignalGenerator.hpp
+/// \brief Classes for the Box Time signal.
+/// \author Yann Renard (Inria).
+/// \version 1.1.
+/// \copyright Copyright (C) 2022 Inria
+///
+/// This program is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU Affero General Public License as published
+/// by the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU Affero General Public License for more details.
+///
+/// You should have received a copy of the GNU Affero General Public License
+/// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+/// 
+///-------------------------------------------------------------------------------------------------
+
 #pragma once
 
-#include "../ovp_defines.h"
+#include "../defines.hpp"
 
 #include <toolkit/ovtk_all.h>
 
@@ -21,7 +44,7 @@ public:
 	bool processClock(Kernel::CMessageClock& msg) override;
 	bool process() override;
 
-	_IsDerivedFromClass_Final_(Toolkit::TBoxAlgorithm<IBoxAlgorithm>, OVP_ClassId_BoxAlgorithm_TimeSignalGenerator)
+	_IsDerivedFromClass_Final_(Toolkit::TBoxAlgorithm<IBoxAlgorithm>, Box_TimeSignalGenerator)
 
 protected:
 	Toolkit::TSignalEncoder<CBoxAlgorithmTimeSignalGenerator> m_encoder;
@@ -44,11 +67,8 @@ public:
 	CString getDetailedDescription() const override { return ""; }
 	CString getCategory() const override { return "Data generation"; }
 	CString getVersion() const override { return "1.1"; }
-	CString getSoftwareComponent() const override { return "openvibe-sdk"; }
-	CString getAddedSoftwareVersion() const override { return "0.0.0"; }
-	CString getUpdatedSoftwareVersion() const override { return "0.4.0"; }
 
-	CIdentifier getCreatedClass() const override { return OVP_ClassId_BoxAlgorithm_TimeSignalGenerator; }
+	CIdentifier getCreatedClass() const override { return Box_TimeSignalGenerator; }
 	IPluginObject* create() override { return new CBoxAlgorithmTimeSignalGenerator(); }
 
 	bool getBoxPrototype(Kernel::IBoxProto& prototype) const override
@@ -61,7 +81,7 @@ public:
 		return true;
 	}
 
-	_IsDerivedFromClass_Final_(IBoxAlgorithmDesc, OVP_ClassId_BoxAlgorithm_TimeSignalGeneratorDesc)
+	_IsDerivedFromClass_Final_(IBoxAlgorithmDesc, Box_TimeSignalGeneratorDesc)
 };
 }  // namespace DataGeneration
 }  // namespace Plugins
--- plugins/processing/feature-extraction/src/ovp_defines.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-// Boxes
-//---------------------------------------------------------------------------------------------------
-#define OVP_ClassId_BoxAlgorithm_FeatureAggregator		OpenViBE::CIdentifier(0x00682417, 0x453635F9)
-#define OVP_ClassId_BoxAlgorithm_FeatureAggregatorDesc	OpenViBE::CIdentifier(0x00B5B638, 0x25821BAF)
