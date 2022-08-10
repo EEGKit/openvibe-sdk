13c015779caa692aef91b8666b2481319c0c8d4f
MONSEIGNE Thibaut <thibaut.monseigne@inria.fr>
2022-08-10 08:03:28 +0000

    :fire: Resolve "Update: remove Date module"

--- modules/date/include/date/WindowsDateParser.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "defines.h"
-#include <ctime>
-
-namespace DateParser {
-extern Date_API char* windowsStrptime(const char* buf, const char* fmt, struct tm* tmParsed);
-}  // namespace DateParser
